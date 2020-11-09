bf91e3a4b56563fbb9521d465133e171de0fe7b7
Simon Stoltze <Simon.Stoltze@ufst.dk>
2020-11-09 23:53:38 +0100

    Blink in morse code

--- rust-arduino-blink/src/main.rs
+++ rust-arduino-blink/src/main.rs
@@ -9,12 +9,36 @@ extern crate panic_halt;
 use arduino_uno::hal::port::mode::Output;
 use arduino_uno::hal::port::portb::PB5;
 use arduino_uno::prelude::*;
+mod morse;
+
+const MORSE_TIME_SCALE: u16 = 300;
 
 fn stutter_blink(led: &mut PB5<Output>, times: usize) {
     (0..times).map(|i| i * 5).for_each(|i| {
         led.toggle().void_unwrap();
         arduino_uno::delay_ms(i as u16);
     })
+}
+
+fn morse_sequence(led: &mut PB5<Output>, seq: [morse::Morse; 6]) {
+    seq.iter().for_each(|m| morse_toggle(led, m));
+}
+
+fn morse_toggle(led: &mut PB5<Output>, m: &morse::Morse) {
+    if !m.pause() {
+        led.toggle().void_unwrap();
+    }
+    arduino_uno::delay_ms((m.timing() * MORSE_TIME_SCALE) as u16);
+    if !m.pause() {
+        led.toggle().void_unwrap();
+    }
+    arduino_uno::delay_ms(MORSE_TIME_SCALE);
+}
+
+fn morse_blink(led: &mut PB5<Output>, sentence: &str) {
+    sentence
+        .chars()
+        .for_each(|c| morse_sequence(led, morse::char_to_morse(c)))
 }
 
 #[arduino_uno::entry]
@@ -26,6 +50,6 @@ fn main() -> ! {
     let mut led = pins.d13.into_output(&mut pins.ddr);
 
     loop {
-        stutter_blink(&mut led, 100);
+        morse_blink(&mut led, "sarah");
     }
 }
---
+++ rust-arduino-blink/src/morse.rs
@@ -0,0 +1,79 @@
+#![no_std]
+#![no_main]
+
+pub enum Morse {
+    Short,
+    Long,
+    Null,
+    LetterBreak,
+    WordBreak,
+}
+
+impl Morse {
+    pub fn timing(&self) -> u16 {
+        morse_timing(self)
+    }
+
+    pub fn pause(&self) -> bool {
+        use Morse::*;
+        match self {
+            Null | LetterBreak | WordBreak => true,
+            _ => false,
+        }
+    }
+}
+
+pub fn char_to_morse(c: char) -> [Morse; 6] {
+    use Morse::*;
+    match c {
+        'a' => [Short, Long, Null, Null, Null, LetterBreak],
+        'b' => [Long, Short, Short, Short, Null, LetterBreak],
+        'c' => [Long, Short, Long, Short, Null, LetterBreak],
+        'd' => [Long, Short, Short, Null, Null, LetterBreak],
+        'e' => [Short, Null, Null, Null, Null, LetterBreak],
+        'f' => [Short, Short, Long, Short, Null, LetterBreak],
+        'g' => [Long, Long, Short, Null, Null, LetterBreak],
+        'h' => [Short, Short, Short, Short, Null, LetterBreak],
+        'i' => [Short, Short, Null, Null, Null, LetterBreak],
+        'j' => [Short, Long, Long, Long, Null, LetterBreak],
+        'k' => [Long, Short, Long, Null, Null, LetterBreak],
+        'l' => [Short, Long, Short, Short, Null, LetterBreak],
+        'm' => [Long, Long, Null, Null, Null, LetterBreak],
+        'n' => [Long, Short, Null, Null, Null, LetterBreak],
+        'o' => [Long, Long, Long, Null, Null, LetterBreak],
+        'p' => [Short, Long, Long, Short, Null, LetterBreak],
+        'q' => [Long, Long, Short, Long, Null, LetterBreak],
+        'r' => [Short, Long, Short, Null, Null, LetterBreak],
+        's' => [Short, Short, Short, Null, Null, LetterBreak],
+        't' => [Long, Null, Null, Null, Null, LetterBreak],
+        'u' => [Short, Short, Long, Null, Null, LetterBreak],
+        'v' => [Short, Short, Short, Long, Null, LetterBreak],
+        'w' => [Short, Long, Long, Null, Null, LetterBreak],
+        'x' => [Long, Short, Short, Long, Null, LetterBreak],
+        'y' => [Long, Short, Long, Long, Null, LetterBreak],
+        'z' => [Long, Long, Short, Short, Null, LetterBreak],
+        '1' => [Short, Long, Long, Long, Long, LetterBreak],
+        '2' => [Short, Short, Long, Long, Long, LetterBreak],
+        '3' => [Short, Short, Short, Long, Long, LetterBreak],
+        '4' => [Short, Short, Short, Short, Long, LetterBreak],
+        '5' => [Short, Short, Short, Short, Short, LetterBreak],
+        '6' => [Long, Short, Short, Short, Short, LetterBreak],
+        '7' => [Long, Long, Short, Short, Short, LetterBreak],
+        '8' => [Long, Long, Long, Short, Short, LetterBreak],
+        '9' => [Long, Long, Long, Long, Short, LetterBreak],
+        '0' => [Long, Long, Long, Long, Long, LetterBreak],
+        ' ' => [WordBreak, Null, Null, Null, Null, LetterBreak],
+        _ => [Null, Null, Null, Null, Null, Null],
+    }
+}
+
+pub fn morse_timing(m: &Morse) -> u16 {
+    use Morse::*;
+    match m {
+        Null => 0,
+        Short => 1,
+        Long => 3,
+        LetterBreak => 3,
+        WordBreak => 7,
+    }
+}
